5fde1b295405d15d295a0104c11f5b758c355437
dyllanwli <messsking@gmail.com>
2020-02-09 09:49:22 +0800

    add warpper

---
+++ userspider/userid/warpper.py
@@ -0,0 +1,13 @@
+import os
+
+
+id = []
+
+with open("user_list.txt", "r") as file:
+    for line in file.readlines():
+        line = line.split(" ")
+        id.append(line[1])
+
+
+with open("user_list_warppered.txt", "w") as file:
+    file.writelines(id)
